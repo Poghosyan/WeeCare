0ea78e9a9f5e29c52f8b1f5aaae1b75780830e8a
Mher Poghosyan <poghosyanusc@gmail.com>
2023-03-15 23:40:20 -0700

    Added docker setup laravel and database population commands

---
+++ app/Http/Controllers/Api/SongsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Songs;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class SongsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $songs = Songs::all();
+        return response()->json([
+            'songs' => $songs
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Songs  $songs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Songs $songs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Songs  $songs
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Songs $songs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Songs  $songs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Songs $songs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Songs  $songs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Songs $songs)
+    {
+        //
+    }
+}
